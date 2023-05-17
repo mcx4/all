1f3f59924e731279fd2fa46b408a0f82a66c6d1e
rob <robboce@gmail.com>
2023-05-17 22:49:30 +0930

    first commit

---
+++ demos/src/main/java/au/edu/tafesa/itstudies/demos/Demos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package au.edu.tafesa.itstudies.demos;
+
+/**
+ *
+ * @author cevallor
+ */
+public class Demos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ test_source_files/src/main/java/au/edu/tafesa/itstudies/test_source_files/Test_source_files.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package au.edu.tafesa.itstudies.test_source_files;
+
+/**
+ *
+ * @author cevallor
+ */
+public class Test_source_files {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
